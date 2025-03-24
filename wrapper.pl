7caa0ec73c5495ae1ea03ababe73f01f91285431
Greg EVA <gregeva@users.noreply.github.com>
2025-03-24 12:18:14 -0400

    Clean up unused and uneeded files

--- wrapper.pl
+++
@@ -1,3 +0,0 @@
-my $params = join(' ', @ARGV);
-#system("perl ./ltl /Users/geva/Downloads/bspa003/11mar-14mar/access_logs/localhost_access_log-twx01-twx-thingworx-0.2025-03-11.txt");
-system("perl ./ltl $params");
